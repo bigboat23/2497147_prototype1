b02598640f682bf23a1859d2036fcecd250e9a32
bigboat23 <117849391+bigboat23@users.noreply.github.com>
2023-10-31 17:57:32 +0200

    4

--- script.js
+++ script.js
@@ -141,89 +141,185 @@ function changeDescriptor(text) {
 
 
 
-
 //DATA
-// Fetch data from the NASA EPIC API and Display the coordinates of Camera
-fetch('https://epic.gsfc.nasa.gov/api/natural')
-  .then(response => response.json())
-  .then(data => {
-    // Extract latitude and longitude data
-    const coordinates = data.map(item => ({
-      lat: item.centroid_coordinates.lat,
-      lon: item.centroid_coordinates.lon,
-    }));
-
-    // Create a scatter plot
-    createScatterPlot(coordinates);
-  })
- // .catch(error => console.error('Error fetching data:', error));
-
-// Create a scatter plot using D3.js
-function createScatterPlot(data) {
+function initExoplanetMap() {
   const width = 800;
   const height = 400;
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-  // Create an SVG container
-  const svg = d3.select('#scatter-plot')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+  // Define the projection
+  const projection = d3.geoOrthographic()
+    .scale(width / 2)
+    .translate([width / 2, height / 2]);
 
-  // Create scales for x and y axes
-  const xScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.lon), d3.max(data, d => d.lon)])
-    .range([margin.left, width - margin.right]);
+  const path = d3.geoPath().projection(projection);
 
-  const yScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.lat), d3.max(data, d => d.lat)])
-    .range([height - margin.bottom, margin.top]);
+  const svg = d3
+    .select("#exoplanet-map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  // Reference the tooltip element
-  const tooltip = document.getElementById('tooltip');
-  const tooltipContent = document.getElementById('tooltip-content');
 
-  // Create circles for each data point
-  svg.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('cx', d => xScale(d.lon))
-    .attr('cy', d => yScale(d.lat))
-    .attr('r', 4) // Radius of the circles
-    .attr('class', 'circle') // Apply the circle class
-    .on('mouseover', (event, d) => {
-      // Show the tooltip
-      tooltip.style.left = event.pageX + 'px';
-      tooltip.style.top = event.pageY + 'px';
-      tooltipContent.textContent = `Lat: ${d.lat}, Lon: ${d.lon}`;
-      tooltip.classList.add('show');
+  svg.append("path")
+    .attr("class", "celestial-sphere")
+    .attr("d", path({ type: "Sphere" }));
+
+  // Data fetching from the NASA EPIC API
+  fetch('https://epic.gsfc.nasa.gov/api/natural')
+    .then(response => response.json())
+    .then(data => {
+      // Extract exoplanet data 
+      const exoplanets = data.map(item => ({
+        lat: item.centroid_coordinates.lat,
+        lon: item.centroid_coordinates.lon,
+        name: item.exoplanet_name,
+        method: item.discovery_method,
+        // Add more data attributes if needed
+      }));
+
+      // Append exoplanet markers
+      const markers = svg.selectAll("circle")
+        .data(exoplanets)
+        .enter()
+        .append("circle")
+        .attr("cx", d => projection([d.lon, d.lat])[0])
+        .attr("cy", d => projection([d.lon, d.lat])[1])
+        .attr("r", 6) 
+        .attr("class", "exoplanet-marker")
+        .on("mouseover", showTooltip)
+        .on("mouseout", hideTooltip)
+        .on("click", showInfoPanel);
+
+      
+      markers.style("fill", d => (d.method === "Radial Velocity") ? "blue" : "red");
+
+      
+      svg.selectAll(".exoplanet-marker")
+        .style("opacity", 0.8);
+
+      // Zoom and pan functionality
+      svg.call(d3.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", zoomed));
+
+      function zoomed(event) {
+        const { transform } = event;
+        svg.attr("transform", transform);
+      }
+
+      // Tooltip functions
+      function showTooltip(event, d) {
+        const tooltip = d3.select("#tooltip");
+        tooltip.html(`Name: ${d.name}<br>Discovery Method: ${d.method}`)
+          .style("left", event.pageX + "px")
+          .style("top", event.pageY + "px")
+          .style("visibility", "visible");
+      }
+
+      function hideTooltip() {
+        d3.select("#tooltip").style("visibility", "hidden");
+      }
+
+      function showInfoPanel(d) {
+        function showInfoPanel(d) {
+          const infoPanel = d3.select("#info-panel");
+          infoPanel.html(`<h2>${d.name}</h2>
+            
+          `);
+        
+         
+          infoPanel.style("visibility", "visible");
+        }
+        
+        /
+        document.addEventListener("click", function (event) {
+          const infoPanel = d3.select("#info-panel");
+          if (!infoPanel.node().contains(event.target)) {
+            infoPanel.style("visibility", "hidden");
+          }
+        });
+      }
     })
-    .on('mouseout', () => {
-      // Hide the tooltip
-      tooltip.classList.remove('show');
-    });
-
-  // Create x and y axes
-  svg.append('g')
-    .attr('transform', `translate(0, ${height - margin.bottom})`)
-    .call(d3.axisBottom(xScale))
-    .attr('class', 'axis'); // Apply the axis class
-
-  svg.append('g')
-    .attr('transform', `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(yScale))
-    .attr('class', 'axis'); // Apply the axis class
-
-  // Add labels for the axes
-  svg.append('text')
-    .attr('x', width / 2)
-    .attr('y', height)
-    .attr('text-anchor', 'middle');
-
-  svg.append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('x', -height / 2)
-    .attr('y', margin.left / 2)
-    .attr('text-anchor', 'middle');
+    .catch(error => console.error("Error fetching data:", error));
 }
+
+window.addEventListener("load", initExoplanetMap);
+
+
+//ExoplanetSizeDistrubution
+ 
+    const exoplanets = [
+      
+    ];
+
+   
+    const margin = { top: 20, right: 30, bottom: 40, left: 60 };
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+  
+    const svg = d3
+      .select("#chart-container")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    //
+    const sizes = exoplanets.map((planet) => planet.size);
+
+  
+    const xScale = d3
+      .scaleBand()
+      .domain(exoplanets.map((planet) => planet.name))
+      .range([0, width])
+      .padding(0.1);
+
+    
+    const yScale = d3.scaleLinear().domain([0, d3.max(sizes)]).nice().range([height, 0]);
+
+
+    svg
+      .selectAll(".bar")
+      .data(exoplanets)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (planet) => xScale(planet.name))
+      .attr("y", (planet) => yScale(planet.size))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (planet) => height - yScale(planet.size))
+      .attr("fill", "steelblue");
+
+    
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale));
+
+   
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(yScale).ticks(5));
+
+    
+    svg
+      .append("text")
+      .attr("class", "x-label")
+      .attr("x", width / 2)
+      .attr("y", height + margin.top)
+      .attr("text-anchor", "middle")
+      .text("Exoplanet Name");
+
+    svg
+      .append("text")
+      .attr("class", "y-label")
+      .attr("x", -height / 2)
+      .attr("y", -margin.left / 2)
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .text("Exoplanet Size");
+
